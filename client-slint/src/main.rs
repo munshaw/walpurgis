96ca332bcc2d4efdcd79ca6e7c9c949aa6b24f11
Brandon Munshaw <brandon.munshaw@live.ca>
2022-06-26 14:24:53 -0400

    Implement Hello Slint!]

--- client-slint/src/main.rs
+++ client-slint/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    MainWindow::new().run();
+}
+
+slint::slint! {
+    MainWindow := Window {
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
 }
