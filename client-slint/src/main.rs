5ce864fe5de441772f32ca380753763ea7a8d593
Brandon Munshaw <brandon.munshaw@live.ca>
2022-06-26 16:03:45 -0400

    Set window dimensions

--- client-slint/src/main.rs
+++ client-slint/src/main.rs
@@ -1,12 +1,25 @@
+const WINDOW_SCALE: i32 = 2;
+const WINDOW_WIDTH: i32 = 144;
+const WINDOW_HEIGHT: i32 = 160;
+
 fn main() {
-    MainWindow::new().run();
+    let window = MainWindow::new();
+
+    window.set_window_scale(WINDOW_SCALE);
+    window.set_window_width(WINDOW_WIDTH);
+    window.set_window_height(WINDOW_HEIGHT);
+
+    window.run();
 }
 
 slint::slint! {
     MainWindow := Window {
-        Text {
-            text: "hello world";
-            color: green;
-        }
+        property<int> window_scale;
+        property<int> window_width;
+        property<int> window_height;
+
+        width: window_width * window_scale * 1px;
+        height: window_height * window_scale * 1px;
+        title: "Walpurgis";
     }
 }
