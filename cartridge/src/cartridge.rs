5489cbe7bb802b85c5e1bf96d046add067a3fe70
Brandon Munshaw <brandon.munshaw@live.ca>
2022-06-26 21:21:52 -0400

    Create Cartridge module

---
+++ cartridge/src/cartridge.rs
@@ -0,0 +1,8 @@
+/// Your game should implement this. Used by player to create final gui.
+pub trait Cartridge {
+    /// Tile size in pixels, returning (width, height).
+    fn get_tile_size() -> (u32, u32);
+
+    // Size of grin in tiles, returning (width, height).
+    fn get_grid_size() -> (u32, u32);
+}
---
+++ cartridge/src/lib.rs
@@ -0,0 +1,2 @@
+/// Your game should implement this. Used by player to create final gui.
+pub mod cartridge;
