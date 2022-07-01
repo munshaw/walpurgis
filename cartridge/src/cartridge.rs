c03b6e4a746ae742506ffcaf6f106409473dccef
Brandon Munshaw <brandon.munshaw@live.ca>
2022-06-30 20:17:15 -0400

    Create all tiles in rust

--- cartridge/src/cartridge.rs
+++ cartridge/src/cartridge.rs
@@ -1,8 +1,8 @@
 /// Your game should implement this. Used by player to create final gui.
 pub trait Cartridge {
     /// Tile size in pixels, returning (width, height).
-    fn get_tile_size(&self) -> (u32, u32);
+    fn get_tile_size(&self) -> (usize, usize);
 
     // Size of grin in tiles, returning (width, height).
-    fn get_grid_size(&self) -> (u32, u32);
+    fn get_grid_size(&self) -> (usize, usize);
 }
--- constants/src/constants.rs
+++ constants/src/constants.rs
@@ -2,8 +2,8 @@ pub const WINDOW_TITLE: &str = "Walpurgis";
 
 pub const WINDOW_SCALE: f32 = 2.0;
 
-pub const TILE_WIDTH: u32 = 8;
-pub const TILE_HEIGHT: u32 = 8;
+pub const TILE_WIDTH: usize = 8;
+pub const TILE_HEIGHT: usize = 8;
 
-pub const GRID_WIDTH: u32 = 20;
-pub const GRID_HEIGHT: u32 = 18;
+pub const GRID_WIDTH: usize = 20;
+pub const GRID_HEIGHT: usize = 18;
