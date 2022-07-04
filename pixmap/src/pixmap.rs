4b9f56d51ec9dc730c6206da94947144e2b4b238
Brandon Munshaw <brandon.munshaw@live.ca>
2022-07-03 21:32:16 -0400

    Minor cleanup in PixMap

--- pixmap/src/pixmap.rs
+++ pixmap/src/pixmap.rs
@@ -3,7 +3,7 @@ use std::collections::{HashMap, HashSet};
 /// Errors for `PixMap`
 #[derive(Debug)]
 pub enum Error {
-    /// PixMap `pixels` string contains a character unregistered in `colours`.
+    /// PixMap `pixels` string contains a character unregistered in `palette`.
     UnregisteredPixel,
 }
 
@@ -14,23 +14,23 @@ pub type Rgba = (u8, u8, u8, u8);
 /// since all tile sprites have the same size. See tests for examples.
 #[derive(Debug)]
 pub struct PixMap {
-    colours: Vec<(char, Rgba)>,
+    palette: Vec<(char, Rgba)>,
     pixels: String,
 }
 
 impl PixMap {
     /// Create new `PixMap`.
     /// # Arguments
-    /// `colours` registers each character with a colour, and `pixels`
+    /// `palette` registers each character with a colour, and `pixels`
     /// are a string of characters, allowing you to draw in ascii.
-    pub fn new(colours: Vec<(char, Rgba)>, pixels: &str) -> Result<Self, Error> {
+    pub fn new(palette: Vec<(char, Rgba)>, pixels: &str) -> Result<Self, Error> {
         let mut colour_set = HashSet::new();
-        colours.iter().for_each(|c| {
+        palette.iter().for_each(|c| {
             colour_set.insert(c.0);
         });
         if pixels.chars().all(|c| colour_set.contains(&c)) {
             Ok(Self {
-                colours,
+                palette: palette,
                 pixels: pixels.to_string(),
             })
         } else {
@@ -41,7 +41,7 @@ impl PixMap {
     /// Uncompress into to raw rgba8 information.
     pub fn to_rgba8(&self) -> Vec<u8> {
         let mut colour_map = HashMap::new();
-        for colour in &self.colours {
+        for colour in &self.palette {
             colour_map.insert(colour.0, colour.1);
         }
 
